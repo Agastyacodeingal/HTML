ae5d13d6463f12c7eba6a1ab8be5d99aa044cee7
Agastyacodeingal <sweetisharad@gmail.com>
2025-02-03 18:58:57 +0530

    Add files via upload

---
+++ Class 3 Data types.py/Casting.py
@@ -0,0 +1,13 @@
+a = "Agastya"
+age = 12
+weight = 36.2
+isstudent = True
+print("the type of a is ",type(a))
+print("the type of age is ",type(age))
+print("the type of weight is ",type(weight))
+print("the type of isstudent  is ",type(isstudent))
+age = str(age)
+print("the type of age is ",type(age))
+weight = int(weight)
+print(weight)
+print("the type of weight is ",type(weight))
---
+++ Class 3 Data types.py/Data types.py
@@ -0,0 +1,8 @@
+a = 5
+print("type of a is ",type(a))
+b = 1.25
+print("type of b is",type(b))
+c = True
+print("type of c is",type(c))
+d = "Codingal"
+print("type of d is",type(d))
