b37e9a2a73f6d7c48f308640cecb6b8f1a659f47
Agastyacodeingal <sweetisharad@gmail.com>
2025-02-13 19:55:30 +0530

    Add files via upload

---
+++ Class 6 - Python operators 2/AND OR.py
@@ -0,0 +1,14 @@
+a = 10
+b = -10
+c = 0
+if a>0 and b>0:
+    print("a and b are greater than 0")
+else:
+    print("Either a and b are false")
+x = 50
+y = 60
+z = -40
+if x>0 or z>0:
+    print("Either x or y is greater than 0")
+else:
+    print("Both x and y are not greater than 0")
---
+++ Class 6 - Python operators 2/True or False.py
@@ -0,0 +1,12 @@
+a = 20
+b = 30
+c = 30
+print(a!=b)
+print(b!=c)
+a = 4
+b = 5
+c = 5
+if (a!=b)!=(b!=c):
+    print("True")
+else:
+    print("False")
