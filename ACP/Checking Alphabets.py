35d1542e735ebe82a6f0e1eeb4bd463573fee677
Agastyacodeingal <sweetisharad@gmail.com>
2025-04-10 15:49:54 +0530

    Add files via upload

---
+++ ACP/Checking Alphabets.py
@@ -0,0 +1,5 @@
+ch = input("Enter the Charecter")
+if ch>="a"and ch<="z":
+    print("The given charecter is an alphabet")
+else:
+    print("The given charecter is not an alphabet")
---
+++ ACP/Square root.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number"))
+sqrt= n**0.5
+print("The square root of the given number is ",sqrt)
