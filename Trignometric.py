051a20923cb6edaacfc2eac14db050c78e46b345
Agastyacodeingal <sweetisharad@gmail.com>
2025-04-30 13:07:49 +0530

    Add files via upload

---
+++ Trignometric.py
@@ -0,0 +1,4 @@
+import math
+print("The sine of 1 is",math.sin(1))
+print("The cos of 1 is",math.cos(1))
+print("The tan of 1 is",math.tan(1))
---
+++ Tuple Product.py
@@ -0,0 +1,5 @@
+a = (2,4,5,7,9) 
+product = 1
+for i in a:
+    product = product*i
+print("The product of tuples are", product)
